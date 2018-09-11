759c9271a02967d0d6652e7013f35cfb64925804
Jose Vivas <josevivas@minis-Mac-mini.local>
2018-09-11 17:48:04 -0400

    Montando Base para Sol

---
+++ MutanX.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.19;
+
+contract MutantFactory {
+    uint adnBase = 23;
+    //  uint dnaDigits = 16;
+    // uint dnaModulus = 10 ** dnaDigits;
+    //Variable de tipo de objeto
+    // struct Zombie {
+    //     string name;
+    //     uint dna;
+    // }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Reto100Days
